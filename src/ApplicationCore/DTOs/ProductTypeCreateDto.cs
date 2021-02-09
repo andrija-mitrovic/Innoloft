8f05e50d07105a33b60e41c76d3b7c3f7e37b023
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2021-02-09 16:49:32 +0100

    ProductType Dto objects and ProductTypesController added

---
+++ src/ApplicationCore/DTOs/ProductTypeCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.DTOs
+{
+    public class ProductTypeCreateDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/WebAPI/Controllers/ProductTypesController.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductTypesController(
+            IUnitOfWork unitOfWork,
+            IMapper mapper
+            )
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
